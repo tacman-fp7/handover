7b3ae9c10f8f34d5af42a3be338943269dba8820
Giulia Vezzani <giulia.vezzani@iit.it>
2016-10-07 17:25:38 +0200

    start coding

--- in-hand-localization/src/in-hand-segmentation/main.cpp
+++ in-hand-localization/src/in-hand-segmentation/main.cpp
@@ -36,25 +36,100 @@ using namespace yarp::math;
 
 
 /*******************************************************************************/
-class pointCloudExtraction : public RFModule, public PortReader
+class pointCloudExtraction : public RFModule
 {
+protected:
+
+    string module_name;
+    string homeContextPath;
+    string savename;
+    string fileFormat;
+    int fileCount;
+
+    int downsampling;
+    double spatial_distance;
+
+    bool flood3d;
+    bool saving;
+
+    BufferedPort<ImageOf<PixelMono> > portDispIn;
+    BufferedPort<ImageOf<PixelRgb> > portImgIn;
+
+    BufferedPort<Bottle> portPointsOut;
+    BufferedPort<ImageOf<PixelRgb> > portDispOut;
+
+    RpcClient portSFM;
+    RpcClient portSeg;
+    RpcServer portRpc;
+
 public:
 
     /*******************************************************************************/
     bool configure(ResourceFinder &rf)
     {
+        module_name = rf.check("module_name", Value("in-hand-segmentation"), "Getting module name").asString();
+
+        homeContextPath = rf.getHomeContextPath().c_str();
+        savename = rf.check("savename", Value("cloud3D"), "Default file savename").asString();
+        saving = rf.check("savingClouds", Value(false), "Toggle save clouds as file").asBool();
+        fileFormat = rf.check("format", Value("off"), "Default file format").asString();
+
+        cout << "Files will be saved in "<< homeContextPath << " folder, as " << savename <<"N." << fileFormat <<", with increasing numeration N"  << endl;
+        fileCount = 0;
+
+        downsampling = std::max(1,rf.check("downsampling",Value(1)).asInt());
+        spatial_distance = rf.check("spatial_distance",Value(0.005)).asDouble();
+
+        cout<<"Opening ports"<<endl;
+
+        portDispIn.open("/" + module_name + "/disp:i");
+        portImgIn.open("/" + module_name + "/img:i");
+
+        portPointsOut.open("/"+module_name+"/pnt:o");
+        portDispOut.open("/"+module_name+"/disp:o");
+
+        portSFM.open("/"+module_name+"/SFM:rpc");
+        portSeg.open("/"+module_name+"/seg:rpc");
+        portRpc.open("/"+module_name+"/rpc:i");
+
+        cout<<"Ports opened"<<endl;
+
+        attach(portRpc);
+
+        flood3d=false;
+
         return true;
     }
 
     /*******************************************************************************/
     bool interruptModule()
     {
+        portDispIn.interrupt();
+        portImgIn.interrupt();
+
+        portPointsOut.interrupt();
+        portDispOut.interrupt();
+
+        portSFM.interrupt();
+        portSeg.interrupt();
+        portRpc.interrupt();
+
         return true;
     }
 
     /*******************************************************************************/
     bool close()
     {
+        portDispIn.close();
+        portImgIn.close();
+
+        portPointsOut.close();
+        portDispOut.close();
+
+        portSFM.close();
+        portSeg.close();
+        portRpc.close();
+
         return true;
     }
 
