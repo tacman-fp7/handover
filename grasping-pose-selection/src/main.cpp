9ea839da18248ee3a22cc576fb0a7ed640d2f19a
Giulia Vezzani <vzzgli@gmail.com>
2016-11-23 16:02:56 +0100

    read grasping poses attached to object models

--- grasping-pose-selection/src/main.cpp
+++ grasping-pose-selection/src/main.cpp
@@ -15,6 +15,12 @@
  * Public License for more details
 */
 
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
 #include <yarp/math/Math.h>
@@ -26,10 +32,12 @@ using namespace yarp::math;
 
 class poseSelection : public RFModule
 {
-    deque<Vectors> poses;
+    vector<Vector> positions;
+    vector<Vector> orientations;
 
     string orientationFileName;
     string positionFileName;
+    string homeContextPath;
     string frame;
 
     bool change_frame;
@@ -38,11 +46,13 @@ class poseSelection : public RFModule
     bool configure(ResourceFinder &rf)
     {
         positionFileName=rf.check("positionFileName", Value("positions.off"), "Default positions file name").asString();
-        orientationFileName=rf.check("orientationFileName", Value("orientations-right.off"), "Default orientations file name").asString();
-        readPoses(orientationFileName, positionFileName);
+        orientationFileName=rf.check("orientationFileName", Value("orientations-left.txt"), "Default orientations file name").asString();
+        homeContextPath=rf.getHomeContextPath().c_str();
 
-        if (frame == "hand")
-            changeEstimatedPoseFrame();
+        readPoses(positionFileName, orientationFileName);
+
+        //if (frame == "hand")
+        //    changeEstimatedPoseFrame();
 
         return true;
     }
@@ -50,9 +60,18 @@ class poseSelection : public RFModule
     /*********************************************************/
     bool updateModule()
     {
-        choosePose();
+        cout<<"positions"<<endl;
+        for (int i=0; i<positions.size(); i++)
+            cout<<positions[i].toString()<<endl;
 
-        return true;
+        cout<<"orientations"<<endl;
+        for (int i=0; i<orientations.size(); i++)
+            cout<<orientations[i].toString()<<endl;
+
+        //if (online)
+        //    showPoses();
+
+        return false;
     }
 
     /*********************************************************/
@@ -61,7 +80,99 @@ class poseSelection : public RFModule
         return true;
     }
 
+    /*********************************************************/
+    bool readPoses(string &positionFileName, string &orientationFileName)
+    {
+        read(positionFileName, "positions");
+        read(orientationFileName, "orientations");
 
+        return true;
+    }
+
+    /*******************************************************************************/
+    bool read(string &filename, const string &tag)
+    {
+        int state=0;
+        int nPoints;
+        char line[255];
+        vector<Vector> points_tmp;
+        if (tag=="positions")
+            positions.clear();
+        else if (tag=="orientations")
+            orientations.clear();
+        Vector point_tmp;
+        if (tag=="positions")
+            point_tmp.resize(6,0.0);
+        else if (tag=="orientations")
+            point_tmp.resize(9,0.0);
+
+        cout<< "In cloud file "<<homeContextPath+"/"+filename<<endl;
+
+        ifstream cloudFile((homeContextPath+"/"+filename).c_str());
+        if (!cloudFile.is_open())
+        {
+            yError()<<"problem opening cloud file!";
+            return false;
+        }
+
+        while (!cloudFile.eof())
+        {
+            cloudFile.getline(line,sizeof(line),'\n');
+            Bottle b(line);
+            Value firstItem=b.get(0);
+            bool isNumber=firstItem.isInt() || firstItem.isDouble();
+
+            if (state==0)
+            {
+                string tmp=firstItem.asString().c_str();
+                std::transform(tmp.begin(),tmp.end(),tmp.begin(),::toupper);
+                if (tmp=="COFF" || tmp=="OFF")
+                    state++;
+            }
+            else if (state==1)
+            {
+                if (isNumber)
+                {
+                    nPoints=firstItem.asInt();
+                    state++;
+                }
+            }
+            else if (state==2)
+            {
+                if (isNumber && (b.size()>=3))
+                {
+                    point_tmp[0]=b.get(0).asDouble();
+                    point_tmp[1]=b.get(1).asDouble();
+                    point_tmp[2]=b.get(2).asDouble();
+                    point_tmp[3]=b.get(3).asDouble();
+                    point_tmp[4]=b.get(4).asDouble();
+                    point_tmp[5]=b.get(5).asDouble();
+
+                    if (tag=="orientations")
+                    {
+                        point_tmp[6]=b.get(6).asDouble();
+                        point_tmp[7]=b.get(7).asDouble();
+                        point_tmp[8]=b.get(8).asDouble();
+                    }
+                    points_tmp.push_back(point_tmp);
+
+                    if (--nPoints<=0)
+                    {
+                        for( size_t i=0; i<points_tmp.size();i++)
+                        {
+                            point_tmp=points_tmp[i];
+                            if (tag =="positions")
+                                positions.push_back(point_tmp);
+                            else if (tag=="orientations")
+                                orientations.push_back(point_tmp);
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 };
 
 int main(int argc,char *argv[])
@@ -73,7 +184,7 @@ int main(int argc,char *argv[])
         return 1;
     }
 
-    inHandSegmentation mod;
+    poseSelection mod;
     ResourceFinder rf;
     rf.setDefaultContext("poseSelection");
     rf.configure(argc,argv);
